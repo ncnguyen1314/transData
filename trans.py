de5c3860b3e14e16c711c025883a8a7d97fa4345
ncnguyen1314 <75525938+ncnguyen1314@users.noreply.github.com>
2022-03-25 12:43:42 +0700

    Initial commit

---
+++ trans.py
@@ -0,0 +1,27 @@
+
+
+import jsonlines
+f = open("or_train.txt", "w", encoding="utf-8")
+
+# with jsonlines.open('test.jsonl', "r", encoding="utf-8") as reader:
+#     #for line in reader.iter():
+#     for line in reader:
+#         for text in (line['messages']):
+
+#             f.write(text['text'])
+#             f.write('\n')
+
+# f.close()
+
+
+####
+import io
+with jsonlines.open('or_train.jsonl') as reader:
+    for line in reader.iter():
+        for text in (line['messages']):
+            sentence = text['text'].encode('utf-8')
+            f.write(text['text'])
+            f.write('\n')
+
+f.close()
+    
